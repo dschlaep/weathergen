270bbdeb33e2eb0034de931563bf7d38962ead37
Alexander Reeder <alexreeder@bmac3170.nlu.unibas.ch>
2016-08-11 16:44:18 +0200

    camelcase name changed, now package can be installed again.

--- R/get_correlations.R
+++ R/get_correlations.R
@@ -1,24 +1,24 @@
 
 #' get_correlations calculate correlations for plot function 5
 #'
-#' @param AnnualMeans This value can be retrieved from getObservedStatistics(...)$AnnualMeans or getSimulatedStatistics(..)$AnnualMeans
+#' @param annual_means This value can be retrieved from getObservedStatistics(...)$AnnualMeans or getSimulatedStatistics(..)$AnnualMeans
 #' @param vars
 #' @export
 #' @examples
-#' obsstats$cors <- get_correlations(AnnualMeans=obssStats$AnnualMeans[-1, -1, , , drop=FALSE])
+#' obsstats$cors <- get_correlations(annual_means=obssStats$AnnualMeans[-1, -1, , , drop=FALSE])
 #' 
 #'
-getCorrelations <- function(AnnualMeans, vars= c("prcp","tmax","tmin")){
-  num_trials <- dim(AnnualMeans)[1]
+get_correlations <- function(annual_means, vars= c("prcp","tmax","tmin")){
+  num_trials <- dim(annual_means)[1]
   
   #Intersites correlations of annual series
-  intersites <- t(combn(dim(AnnualMeans)[2], 2))
+  intersites <- t(combn(dim(annual_means)[2], 2))
   cors <- array(NA, c(1 + num_trials, nrow(intersites), length(vars)), dimnames=list(NULL, NULL, vars))
   
   for(k1 in 1:num_trials){
     for(is in 1:nrow(intersites)){
       for(iv in 1:length(vars)){
-        cors[1 + k1, is, iv] <- cor(AnnualMeans[k1, intersites[is, 1], iv, ], AnnualMeans[k1, intersites[is, 2], iv, ], method="spearman")
+        cors[1 + k1, is, iv] <- cor(annual_means[k1, intersites[is, 1], iv, ], annual_means[k1, intersites[is, 2], iv, ], method="spearman")
       }
     }
   }
@@ -29,11 +29,11 @@ getCorrelations <- function(AnnualMeans, vars= c("prcp","tmax","tmin")){
   #intersites <- cbind(intersites, cors[1, , , drop=TRUE])
   
   #Lag-1 autocorrelation of annual series
-  lag1auto <- array(NA, c(1 + num_trials, dim(AnnualMeans)[2], length(vars)), dimnames=list(NULL, NULL, vars))
+  lag1auto <- array(NA, c(1 + num_trials, dim(annual_means)[2], length(vars)), dimnames=list(NULL, NULL, vars))
   for(k1 in 1:num_trials){
-    for(is in 1:dim(AnnualMeans)[2]){
+    for(is in 1:dim(annual_means)[2]){
       for(iv in 1:length(vars)){
-        lag1auto[1 + k1, is, iv] <- acf(AnnualMeans[k1, is, iv, ], type="correlation", plot=FALSE)[1, ]$acf
+        lag1auto[1 + k1, is, iv] <- acf(annual_means[k1, is, iv, ], type="correlation", plot=FALSE)[1, ]$acf
         stopifnot(!is.na(lag1auto[1 + k1, is, iv]))
       }
     }
