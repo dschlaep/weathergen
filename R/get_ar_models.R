82863ce46c54c60d4b762d3244cee85a06e6e26a
Alexander Reeder <alexreeder@bmac3170.nlu.unibas.ch>
2016-08-19 10:50:27 +0200

    Add get_ar_models as helper function for plot_gws_power. Did not find example how to use this plot function otherwise.

--- R/get_ar_models.R
+++ R/get_ar_models.R
@@ -1,8 +1,20 @@
-get_ar_models <- function(annual_prcp, years, Wavelet_Decomposition, use_warm_model = TRUE){
+#' Get ARIMA Models
+#'
+#' @param annual_prcp yearly prcp 
+#' @param years  the years of the annual prcp data
+#' @param wavelet_decomposition, can be retrieved using wavelet_components() 
+#' @param use_warm_model boolean if warm modelis used
+#' @export
+#' @examples
+#' ar_models <- get_ar_models( mysimStats$AnnualMeans[1,1,1,], names(mysimStats$AnnualMeans[1,1,1,]), wavelet_components(mysimStats$AnnualMeans[1,1,1,],wavelet_analysis(mysimStats$AnnualMeans[1,1,1,], names(mysimStats$AnnualMeans[1,1,1,]),sig.level=0.9, "white"), n.periods=1, sig.periods=c(8,9,10,11,12,13,14,15), n.comp.periods=8))
+#' 
+#' 
+#'
+get_ar_models <- function(annual_prcp, years, wavelet_decomposition, use_warm_model = TRUE){
   ar_models <- list()
   if (use_warm_model) {
-    for (k in 1:dim(Wavelet_Decomposition)[2]) {
-      ar_models[[k]] <- auto.arima(Wavelet_Decomposition[,k],max.p=2,max.q=2,max.P=0,max.Q=0,stationary=TRUE)
+    for (k in 1:dim(wavelet_decomposition)[2]) {
+      ar_models[[k]] <- auto.arima(wavelet_decomposition[,k],max.p=2,max.q=2,max.P=0,max.Q=0,stationary=TRUE)
     }
   } else {
     ar_models[[1]] <- auto.arima(annual_prcp,max.p=2,max.q=2,max.P=0,max.Q=0,stationary=TRUE)
